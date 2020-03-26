a4ad8f8f5e6ea30a442f9b40ef5a387fd374239c
Rolando Lopantzi <rlopantzi@gmail.com>
2020-03-26 12:38:30 -0700

    init commit

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,14 @@ AOS.init({
     duration: 850
 });
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 const socket = io();
 
 // Messages from server
 socket.on('message', message => {
     console.log(message);
     outputMessage(message);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 })
 
 // Message submit
@@ -22,4 +24,6 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
+    div.innerHTML = `<p class="meta bold">Rolando <span>12:35pm</span></p><p class="message-text">${message}</p>`;
+    document.querySelector('.chat-messages').appendChild(div);
 }
